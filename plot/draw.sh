67947842bd7e0acc4e8974fd3da4d9cb97025d7d
Mingfu Shao <shaomingfu@gmail.com>
2020-03-03 22:02:52 -0500

    update

--- plot/draw.sh
+++ plot/draw.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+R CMD BATCH time.R
+./wrap.sh time.tex
+pdflatex time.tex
+
+pdflatex combine.tex
+exit
+
+
 cat tex.R | sed 's/AAA/gtex.list/g' | sed 's/BBB/gtex.tex/g' > gtex.R
 R CMD BATCH gtex.R
 ./wrap.sh gtex.tex
 pdflatex gtex.tex
 
 
+
 cat tex.R | sed 's/AAA/egeuv6.list/g' | sed 's/BBB/egeuv6.tex/g' > egeuv6.R
 R CMD BATCH egeuv6.R
 ./wrap.sh egeuv6.tex
 pdflatex egeuv6.tex
-
-pdflatex combine.tex
-
-exit
 
 cat tex.R | sed 's/AAA/mixed.list/g' | sed 's/BBB/mixed.tex/g' > mixed.R
 R CMD BATCH mixed.R
