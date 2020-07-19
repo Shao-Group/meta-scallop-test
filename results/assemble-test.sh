c379c24273aaabcb68423ef3be169b75f057f736
Mingfu Shao <shaomingfu@gmail.com>
2020-07-19 16:01:55 -0400

    update scripts

--- results/assemble-test.sh
+++ results/assemble-test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 #id="sra-hisat-ref-A02"
-id="gtex2377-A21"
-list=/gpfs/group/mxs2589/default/qqz5133/gtex/meta-scallop/bamlist/2377.txt
+id="gtex20-T20A"
+metadir=/storage/home/mxs2589/shared/projects/aletsch
+
+list=/gpfs/group/mxs2589/default/qqz5133/gtex/meta-scallop/bamlist/20.txt
 chrm=/gpfs/group/mxs2589/default/qqz5133/gtex/meta-scallop/bamlist/chr.list
 
 cores="20"
@@ -20,7 +22,6 @@ ref=/storage/home/mxs2589/shared/data/gencode/GRCh38/gencode.v33.annotation.gtf
 #ref=/gpfs/group/mxs2589/default/shared/data/ensembl/release-97/GRCh38/Homo_sapiens.GRCh38.97.gtf
 
 #metadir=/home/mxs2589/shao/project/aletsch
-metadir=/storage/home/mxs2589/shared/projects/aletsch
 cd $metadir
 mid=`git log | grep commit | head -n 1 | cut -f 2 -d " " | cut -c 1-6`
 meta=$cur/aletsch-$mid
@@ -35,7 +36,7 @@ dagfile=$pbsdir/submit.dag
 pbsfile=$pbsdir/profile.pbs
 echo "#!/bin/bash" > $pbsfile
 echo "#PBS -l nodes=1:ppn=$cores" >> $pbsfile
-echo "#PBS -l mem=180gb" >> $pbsfile
+echo "#PBS -l mem=120gb" >> $pbsfile
 echo "#PBS -l walltime=100:00:00" >> $pbsfile
 echo "#PBS -A mxs2589_b_g_sc_default" >> $pbsfile
 echo "$dir/aletsch-profile.sh $id $threads $list $cur profiles $meta" >> $pbsfile
@@ -51,7 +52,7 @@ do
 	pbsfile=$pbsdir/$kk
 	echo "#!/bin/bash" > $pbsfile
 	echo "#PBS -l nodes=1:ppn=$cores" >> $pbsfile
-	echo "#PBS -l mem=180gb" >> $pbsfile
+	echo "#PBS -l mem=120gb" >> $pbsfile
 	echo "#PBS -l walltime=100:00:00" >> $pbsfile
 	echo "#PBS -A mxs2589_b_g_sc_default" >> $pbsfile
 	echo "$dir/aletsch-chrm.sh $id $threads $list $k $cur/profiles $cur/$kk $meta $ref" >> $pbsfile
@@ -66,7 +67,7 @@ mergefile=$pbsdir/merge.pbs
 echo "#!/bin/bash" > $mergefile
 #echo "#PBS -W depend=$dep" >> $mergefile
 echo "#PBS -l nodes=1:ppn=$cores" >> $mergefile
-echo "#PBS -l mem=180gb" >> $mergefile
+echo "#PBS -l mem=120gb" >> $mergefile
 echo "#PBS -l walltime=100:00:00" >> $mergefile
 echo "#PBS -A mxs2589_b_g_sc_default" >> $mergefile
 echo "$dir/aletsch-combine.sh $id $threads $list $chrm $cur $ref" >> $mergefile
@@ -76,8 +77,8 @@ dag2="$dag2 CHILD combine"
 echo $dag1 >> $dagfile
 echo $dag2 >> $dagfile
 
-module use /gpfs/group/dml129/default/sw/modules
-module load pbstools
-dagsub $dagfile
+#module use /gpfs/group/dml129/default/sw/modules
+#module load pbstools
+#dagsub $dagfile
 
 cd -
